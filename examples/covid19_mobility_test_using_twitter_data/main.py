ed57feed6b33345e8548ca58b96eb939f35125bd
Jorge V. C <jorgevc@fcfm.buap.mx>
2021-08-27 13:01:56 -0500

    Submited version to Statistical Computing (2021) ssssssssssssssssssssssssssssssssssssssssss

--- examples/covid19_mobility_test_using_twitter_data/main.py
+++ examples/covid19_mobility_test_using_twitter_data/main.py
@@ -16,14 +16,14 @@ import copy
 import random
 
 def f(x,n_links):
-    y=1./(1.+x)**1.0326
+    y=1./(1.+x)**1.24
     A=sum(y)
     z=np.rint(((n_links)/A)*y)
     return (x,z)
     #return 40/(1+x)**1.0326  precovid
 
 def g(x,n_links):
-    y=1./(1.+x)**0.7905
+    y=1./(1.+x)**0.8952
     A=sum(y)
     z=np.rint(((n_links)/A)*y)
     return (x,z)
@@ -38,16 +38,19 @@ def distance_matrix(n,d_max):
     # Matriz de ceros
     Z = np.zeros((n,n))
 
+    position = {}
+
     #Distancias entre todos los puntos
     for i in range(n):
         for j in range(n):
             s = (X[i] - X[j], Y[i] - Y[j])
             d_ij = np.linalg.norm(s)
             Z[i][j] = d_ij
+        position[i]=(X[i],Y[i])
 
-    Z1 = np.triu(Z)
-
-    return Z1
+    #Z1 = np.triu(Z)
+    #return Z1
+    return Z,position
 
 def adjacency_matrixt(n,matrix,d_distribution):
     # Lista de Matrices adyacentes
@@ -55,7 +58,7 @@ def adjacency_matrixt(n,matrix,d_distribution):
     bins = d_distribution[0]
 
     for i in range(len(bins)-1):
-        # Índices de los datos que que están el el intervalo [i,i+1)
+        # Índices de los datos que están el intervalo [i,i+1)
         condition = ((matrix>=bins[i]) & (matrix<bins[i+1]))
         index = np.array(np.where( condition )).T
         total_index = len(index)
@@ -69,24 +72,30 @@ def adjacency_matrixt(n,matrix,d_distribution):
             else:
                 adj+=condition.astype(int)
 
-    adj+=adj.T
+    #adj+=adj.T
 
     return adj
 
 def from_geometric_network(nodes,d_distribution):
     d_max=d_distribution[0][-1]
-    dist_m = distance_matrix(nodes,d_max)
+    dist_m,position = distance_matrix(nodes,d_max)
     adj_mt = adjacency_matrixt(nodes, dist_m, d_distribution)
-    return adj_mt
+    return adj_mt,position
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
+if (True):
     # Vectores de los ejes x y y
     import time
-    n = 100 #patches
-    d_max = 10
-    #n_links = 50*2.9
-    Rel=0.8
+    n = 30 #patches
+    d_max = 15
+    #n_links = np.array([int(0.5*n),int(n),int(1.5*n),int(2*n),int(2.5*n),int(3*n)])
+    n_links = [int(1.2*n)]
+    #Rel=0.8 reduction in links 1st attempt
+    Ren=0.71 #reduction in visited sites
+    #Rel=2.1 #1.9 #reduction or increment in visited sites douring covid
+    Rel=1.
+
     bins = np.arange(0.5,d_max)
 
 
@@ -114,7 +123,7 @@ if __name__ == '__main__':
     infection_size_normal=[]
     infection_peak_covid=[]
     infection_size_covid=[]
-    n_links = np.arange(30,401,10)
+    #n_links = np.arange(30,401,10)
     point4=0.
     for l in n_links:
         y[1] = 0.0
@@ -136,17 +145,42 @@ if __name__ == '__main__':
         d_dist_Covid=g(bins,l*Rel)
 
         for i in range(number_of_simulations) :
-            adj_matrix=from_geometric_network(n,d_dist_preCovid)
+            adj_matrix,position=from_geometric_network(n,d_dist_preCovid)
             P = MobilityNetwork()
             P.from_adjacency_matrix(adj_matrix, 0.8) #para cada simulacion se genera nueva mobilidad
+            #P.add_nodes_property("position",position)
+            if i==0:
+                nx.draw(P.network, pos=position,arrows=False, with_labels=True)
+                plt.show()
+                G_deg = nx.degree_histogram(P.network)
+                G_deg_sum = [a * b for a, b in zip(G_deg, range(0, len(G_deg)))]
+                print('average degree: {}'.format(sum(G_deg_sum) / P.network.number_of_nodes()))
+
             sim.set_conectivity_network(P)
             del(P)
             ensemble.add_simulation(sim)
-            adj_matrix2=from_geometric_network(n,d_dist_Covid)
+            n_2=int(n*Ren)
+            adj_tmp,position=from_geometric_network(n_2,d_dist_Covid)
+            P_aux = MobilityNetwork()
+            P_aux.from_adjacency_matrix(adj_tmp, 0.8)
+            if i==0:
+                nx.draw(P_aux.network, pos=position,arrows=True, with_labels=True)
+                plt.show()
+                G_deg = nx.degree_histogram(P_aux.network)
+                G_deg_sum = [a * b for a, b in zip(G_deg, range(0, len(G_deg)))]
+                print('average degree Covid: {}'.format(sum(G_deg_sum) / P_aux.network.number_of_nodes()))
+            X = np.random.randint(d_max, size = n-n_2)
+            Y = np.random.randint(d_max, size = n-n_2)
+            for j in range(n-n_2):
+                position[n_2+j]=(X[j],Y[j])
+            adj_matrix2=np.zeros((n,n))
+            adj_matrix2[:n_2,:n_2]=adj_tmp
             P = MobilityNetwork()
             P.from_adjacency_matrix(adj_matrix2, 0.8) #para cada simulacion se genera nueva mobilidad
+
             sim.set_conectivity_network(P)
             ensemble_Covid19.add_simulation(sim)
+            exit()
 
         ensemble.multiprocessing_run_all(8) #12 is the number of processes
         ensemble_Covid19.multiprocessing_run_all(8)
@@ -160,18 +194,30 @@ if __name__ == '__main__':
         ensemble_Covid19.average_suseptible()
         R = ensemble_Covid19.susceptible_average[0] - ensemble_Covid19.susceptible_average[-1]
         infection_size_covid.append(R)
+        #print('infection size preCovid: {}'.format(infection_size_normal[0]/30000.))
+        #print('infection size Covid: {}'.format(infection_size_covid[0]/30000.))
+
         print("procesado de 300", l)
 
-    np.save('infection_peak_normal',np.array(infection_peak_normal))
-    np.save('infection_size_normal',np.array(infection_size_normal))
-    np.save('infection_peak_covid',np.array(infection_peak_covid))
-    np.save('infection_size_covid',np.array(infection_size_covid))
-    np.save('n_links',n_links)
+    np.save('infection_peak_normal_n',np.array(infection_peak_normal))
+    np.save('infection_size_normal_n',np.array(infection_size_normal))
+    np.save('infection_peak_covid_n',np.array(infection_peak_covid))
+    np.save('infection_size_covid_n',np.array(infection_size_covid))
+    np.save('n_links_n',n_links)
 
-    # axes = ensemble.plot_infected_average()
+    # axes = ensemble.plot_infected_average(relative=True)
     # axes.lines[0].set_label('Normal mobility')
-    # axes1 = ensemble_Covid19.plot_infected_average(axes)
-    # axes1.lines[1].set_label('Stay at home campaing')
+    # axes1 = ensemble_Covid19.plot_infected_average(axes,relative=True)
+    # axes1.lines[1].set_label('Reduced mobility')
+    # axes1.set_xlabel('Time (days)')
+    # #
+    # plt.legend()
+    # plt.show()
     #
+    # axes = ensemble.plot_recovered_average(relative=True)
+    # axes.lines[0].set_label('Normal mobility')
+    # axes1 = ensemble_Covid19.plot_recovered_average(axes,relative=True)
+    # axes1.lines[1].set_label('Reduced mobility')
+    # axes1.set_xlabel('Time (days)')
     # plt.legend()
     # plt.show()
--- examples/covid19_mobility_test_using_twitter_data/plots.py
+++ examples/covid19_mobility_test_using_twitter_data/plots.py
@@ -6,23 +6,50 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.interpolate import CubicSpline
 import csaps
 
-peak_covid=np.load('infection_peak_covid.npy')
-peak_normal=np.load('infection_peak_normal.npy')
-size_covid=np.load('infection_size_covid.npy')
-size_normal=np.load('infection_size_normal.npy')
-links=np.load('n_links.npy')
+peak_covid=np.load('infection_peak_covid_n.npy')
+peak_normal=np.load('infection_peak_normal_n.npy')
+size_covid=np.load('infection_size_covid_n.npy')
+size_normal=np.load('infection_size_normal_n.npy')
+links=np.load('n_links_n.npy')
 
-n=100.
+n=30 #100.
 S=1000.
 Norm=100./(n*S)
 
 x=links
-y=peak_normal-peak_covid
+# Peak
+# y=(1. - peak_covid/peak_normal)*100.
+# spl= UnivariateSpline(x,y, s=1490000)
+# #cs = CubicSpline(links,peak_normal-peak_covid)
+#
+# plt.plot(links/n,peak_normal*Norm,'o',label="base line mobility")
+# plt.plot(links/n,peak_covid*Norm,'+',label="reduced mobility")
+#
+# plt.xlabel("links/node")
+# plt.ylabel("% of infected population")
+# plt.legend()
+#
+# plt.figure()
+#
+#
+# xs = np.linspace(15,90,50)
+#
+#
+# plt.plot(links/n, (1. - peak_covid/peak_normal)*100.,'o',label="simulations data")
+# plt.plot(xs/n,spl(xs),label="spline interpolation")
+# plt.xlabel("links per node")
+# plt.ylabel( "% of reduced infections")
+# plt.legend()
+#
+# plt.show()
+
+#size
+y=(1. - size_covid/size_normal)*100.
 spl= UnivariateSpline(x,y, s=1490000)
 #cs = CubicSpline(links,peak_normal-peak_covid)
 
-plt.plot(links/n,peak_normal*Norm,'o',label="base line mobility")
-plt.plot(links/n,peak_covid*Norm,'+',label="stay at home mobility")
+plt.plot(links/n,size_normal*Norm,'o',label="base line mobility")
+plt.plot(links/n,size_covid*Norm,'+',label="reduced mobility")
 
 plt.xlabel("links/node")
 plt.ylabel("% of infected population")
@@ -31,11 +58,11 @@ plt.legend()
 plt.figure()
 
 
-xs = np.linspace(30,400,100)
+xs = np.linspace(15,90,50)
 
 
-plt.plot(links/n,Norm*(peak_normal-peak_covid),'o',label="simulations data")
-plt.plot(xs/n,Norm*spl(xs),label="spline interpolation")
+plt.plot(links/n, (1. - size_covid/size_normal)*100.,'o',label="simulations data")
+plt.plot(xs/n,spl(xs),label="spline interpolation")
 plt.xlabel("links per node")
 plt.ylabel( "% of reduced infections")
 plt.legend()

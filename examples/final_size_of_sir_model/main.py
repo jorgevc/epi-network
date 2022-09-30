7a3472c824aa20cabe4e314c2e67ed14c946d685
Jorge Velazquez-Castro <jorge.velazquezcastro@correo.buap.mx>
2022-09-29 22:03:25 -0500

    Implemented final size SIR model example

---
+++ examples/final_size_of_sir_model/main.py
@@ -0,0 +1,90 @@
+#
+#  Copyright 2022 Jorge Velazquez Castro
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+import numpy as np
+
+from epinetwork.Simulation import simulation
+from epinetwork.Models import SIR
+from epinetwork.MobilityNetwork import MobilityNetwork
+from epinetwork.Control_protocol import controlProtocol
+from epinetwork.SimulationsEnsemble import simulationsEnsemble
+from epinetwork.Control_protocol import RandomControl, IndexBasedControl, noControl
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def index():
+    n = 4 #numero de parches
+    b = 0.5 # parametro de la red binomial
+    min_residential = 0.9 # diagonal de la matriz de mobilidad mayor a este numero
+    #vector de parametros para una zona
+    param = np.zeros(2)
+    param[0] = beta = 0.67 # 0.67
+    param[1] = gamma = 1./7. #1./7.
+
+    #initial conditions para una zona
+    y = np.zeros(3)
+    S = y[0] = 35000 #1500. 25000
+    I = y[1] = 0.0
+    R = y[2] = 0.0
+
+    #P = MobilityNetwork()
+    #P.barabsi_albert(n,m=2,min_residential=min_residential)
+    #P.binomial(n,b,min_residential=min_residential)
+    sirModel = SIR(n,params=param)
+    P = MobilityNetwork()
+    #P.barabsi_albert(n,m=2,min_residential=min_residential)
+    b = 0.5 # parametro de la red binomial
+    min_residential = 0.9 # diagonal de la matriz de mobilidad mayor a este numero
+    P.binomial(n,b,min_residential=min_residential)
+    sirModel.set_network(P)
+
+    sim = simulation(sirModel)
+    sim.set_initial_conditions_all_patches(y)
+    N = sim.node[:,0]
+    y[1]=y[1]+1. # Se agrega 1 infectado a las condiciones iniciales
+    sim.set_initial_conditions_patch(0,y) #Se establece esta condicion inicial en la zona 1
+    sim.run()
+    sim.plot_all()
+
+    R_inf = []
+    iteration = range(1,10)
+
+    for it in iteration:
+        sirModel.final_size_max_iterations = it
+        R_inf.append(sirModel.local_final_size(N,N).copy())
+    R_inf = np.array(R_inf)
+
+    fig, ax = plt.subplots(2,2)
+    ax1=ax.flatten()
+    for patch in range(sirModel.number_of_patches):
+
+         ax1[patch].scatter(iteration,R_inf[:,patch])
+         ax1[patch].axhline(y=sim.evolution[patch,2,-1],ls='--', label=r'$R_{inf}$')
+
+         ax1[patch].legend()
+         ax1[patch].set_xlabel("No. of iterations")
+         ax1[patch].set_ylabel(r'$R_{inf}$')
+         #ax1.set_title(r'$R(\infty)$ vs  $\mu/\beta$' )
+
+
+    plt.show()
+
+if (True):
+    index()
